c8ac2f6ef895a22a2130fa2a182e056679606a49
Ly Thanh Dat <dat6499proo@gmail.com>
2021-05-26 16:32:46 +0700

    add icon brand

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <!-- Header -->
-    <navbar id="myHeader" class="navbar container"></navbar>
+    <navbar
+      style="z-index:9999"
+      id="myHeader"
+      class="navbar container"
+    ></navbar>
     <!-- Body -->
     <v-card
       tile
@@ -10,7 +14,62 @@
       class="container body-container row-style"
       style="display:block;"
     >
-      <b-row style="width:100%;" no-gutters="true">
+      <div
+        style="justify-content:flex-start;display:flex;z-index:1000;position:absolute;height:20px;width:100%;background-color:black;opacity:0.6;color:white"
+      >
+        <span style="color: #F6DC24;margin-left:10px"
+          ><strong>TRENDING ON LC</strong></span
+        >
+        <b-link class="trending-link" style="margin-left:10px"
+          >Eternals Trailer
+        </b-link>
+        <b-link class="trending-link" style="margin-left:10px"
+          >Disney Animation Showdown
+        </b-link>
+
+        <b-link class="trending-link" style="margin-left:10px"
+          >TV Premiere Dates
+        </b-link>
+        <b-link class="trending-link" style="margin-left:10px"
+          >The Rotten Tomatoes Channel
+        </b-link>
+        <b-link
+          style="text-decoration:none;margin-left: auto;margin-right:10px"
+        >
+          <v-icon dense small style="color:white;">
+            mdi-facebook
+          </v-icon>
+        </b-link>
+        <b-link
+          style="text-decoration:none;margin-left: 10px;margin-right:10px"
+        >
+          <v-icon dense small style="color:white;">
+            mdi-snapchat
+          </v-icon>
+        </b-link>
+        <b-link
+          style="text-decoration:none;margin-left: 10px;margin-right:10px"
+        >
+          <v-icon dense small style="color:white;">
+            mdi-instagram
+          </v-icon>
+        </b-link>
+        <b-link
+          style="text-decoration:none;margin-left: 10px;margin-right:10px"
+        >
+          <v-icon dense small style="color:white;">
+            mdi-twitter
+          </v-icon>
+        </b-link>
+        <b-link
+          style="text-decoration:none;margin-left: 10px;margin-right:10px"
+        >
+          <v-icon dense small style="color:white;">
+            mdi-youtube
+          </v-icon>
+        </b-link>
+      </div>
+      <b-row style="width:100%;">
         <b-col lg="6">
           <carousel-home style="width:100%" />
         </b-col>
@@ -18,6 +77,7 @@
         <b-col lg="3"><news-home style="width:100%"/></b-col>
       </b-row>
     </v-card>
+
     <!-- Footer -->
     <div></div>
   </div>
@@ -81,5 +141,13 @@ export default {
 }
 .row {
   --bs-gutter-x: 0 !important;
+}
+.trending-link {
+  text-decoration: none;
+  color: white;
+}
+.trending-link:hover {
+  text-decoration: none;
+  color: #f6dc24;
 }
 </style>
