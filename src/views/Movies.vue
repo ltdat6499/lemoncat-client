cc651ee2ea201b40075831cf641602e9a7bb4ee1
Ly Thanh Dat <dat6499proo@gmail.com>
2021-07-07 20:11:54 +0700

    doing movies

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -290,33 +290,34 @@
             </div>
             <!-- Filter Score -->
             <div
+              @click="show.range = !show.range"
               style="float:left;color:gray;border: 1px solid #CED4DA;padding:4px;border-radius: 4px;cursor:pointer"
             >
-              <img
-                src="https://live.staticflickr.com/65535/51280469111_d17f4e62ea_o.png"
-                style="width:20px; height:20px"
-              />
-              <span>4%</span>
+              <img :src="valueIcon[0]" style="width:20px; height:20px" />
+              <span>{{ value[0] }}%</span>
               <span> - </span>
-              <img
-                src="https://live.staticflickr.com/65535/51280469111_d17f4e62ea_o.png"
-                style="width:20px; height:20px"
-              />
-              <span>100% <v-icon>mdi-menu-down</v-icon></span>
+              <img :src="valueIcon[1]" style="width:20px; height:20px" />
+              <span>{{ value[1] }}% <v-icon>mdi-menu-down</v-icon></span>
             </div>
             <!-- Filter Genres -->
             <div
+              @click="show.genre = !show.genre"
               style="margin-left:10px;float:left;color:gray;border: 1px solid #CED4DA;padding:4px;border-radius: 4px;cursor:pointer"
             >
               <span
-                >Genres: <span style="color:black"><strong>ALL</strong></span>
+                >Genres:
+                <span style="color:black"
+                  ><strong>{{
+                    selected.length ? selected.length : "ALL"
+                  }}</strong></span
+                >
                 <v-icon>mdi-menu-down</v-icon></span
               >
             </div>
             <!-- Options -->
             <div
-              v-if="true === false"
-              style="padding: 5px; border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 404px; background-color: white;"
+              v-if="show.range"
+              style="padding: 5px; border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 404px; background-color: white;z-index:999"
             >
               <strong>LEMONCATER</strong>
               <div class="app-content">
@@ -330,7 +331,7 @@
                 ></vue-range-slider>
               </div>
               <v-checkbox
-                v-model="selected"
+                v-model="showFreshOnly"
                 color="success"
                 value="success"
                 hide-details
@@ -344,8 +345,8 @@
               </v-checkbox>
             </div>
             <div
-              v-if="true === false"
-              style="padding: 10px; border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 171px; background-color: white;"
+              v-if="show.genre"
+              style="padding: 10px; border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 171px; background-color: white;z-index:999"
             >
               <div v-for="item in genres" :key="item">
                 <v-checkbox
@@ -360,19 +361,24 @@
             </div>
             <!-- Right -->
             <div
-              style="float:right;margin-left:auto;color:gray;border: 1px solid #CED4DA;padding:4px;border-radius: 4px;cursor:pointer"
+              @click="show.date = !show.date"
+              style="float:right;margin-left:auto;color:gray;border: 1px solid #CED4DA;padding:4px;border-radius: 4px;cursor:pointer;width:193px"
             >
               <span
-                >Sort by: <strong>Release Date</strong>
+                >Sort by: <strong>{{ sort }}</strong>
                 <v-icon>mdi-menu-down</v-icon></span
               >
             </div>
             <div
-              v-if="true === false"
-              style="border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 10px; background-color: white; width: 193px;"
+              v-if="show.date"
+              style="border-radius: 4px; border: 1px solid gray; position: absolute; top: 151px; right: 10px; background-color: white; width: 193px;z-index:999"
             >
-              <div class="sort-button">Release Date</div>
-              <div class="sort-button">Lemoncater</div>
+              <div @click="sort = 'Release Date'" class="sort-button">
+                Release Date
+              </div>
+              <div @click="sort = 'Lemoncater'" class="sort-button">
+                Lemoncater
+              </div>
             </div>
           </div>
           <!-- View Mode -->
@@ -475,9 +481,9 @@
 </template>
 
 <script>
+import getFlimsByParams from "@/apollo/queries/getFlimsByParams.gql";
 import "vue-range-component/dist/vue-range-slider.css";
 import VueRangeSlider from "vue-range-component";
-// import getAllUsers from "@/apollo/queries/getAllUsers.gql";
 import Navbar from "@/components/Navbar";
 import PageFooter from "@/components/Footer";
 import HeaderBar from "@/components/Movie/HeaderBar";
@@ -498,23 +504,82 @@ export default {
   },
   data() {
     return {
+      show: {
+        range: false,
+        genre: false,
+        date: false
+      },
       slug: "",
       currentPage: 1,
       rows: 32000,
       perPage: 20,
       viewMode: 0,
+      showFreshOnly: false,
+      sort: "Release Date",
       selected: [],
-      genres: "Action, Adventure, Comedy, Crime and mystery, Fantasy, Historical, Historical fiction, Horror, Romance, Satire, Science fiction, Cyberpunk and derivatives, Speculative, Thriller, Western".split(
-        ", "
-      ),
+      genres: [
+        "Sci-fi",
+        "Mystery & Thriller",
+        "Comedy",
+        "Drama",
+        "Horror",
+        "Adventure",
+        "Fantasy",
+        "Action",
+        "Documentary",
+        "Kids & Family",
+        "Biography",
+        "Animation",
+        "Crime",
+        "War",
+        "Romance",
+        "Western",
+        "Music",
+        "Anime",
+        "Unknown",
+        "Musical",
+        "History",
+        "Sports & fitness",
+        "Other",
+        "Stand-up"
+      ],
       status: false,
-      value: [0, 100],
+      value: [1, 99],
       filter: "",
       tagsString:
         "gameofthrones FirstLook werewolf Spectrum Originals DC Comics GLAAD CBS All Access laika anthology adaptation GIFs TNT hist Tarantino 007 TCM ghosts justice league 2016 El Rey zombie TCA Winter 2020 Super Bowl Amazon Studios series MCU Disney The Academy festival Endgame Film Festival Best and Worst NBC IFC Films parents Emmy Nominations stop motion jurassic park TV renewals Teen Turner Classic Movies criterion Toys BBC One finale Food Network popular documentary A24 IFC Trivia kids Pop stand-up comedy science fiction cats Holidays cancelled television book adaptation richard e. Grant child's play 2021 Disney Channel The CW Dark Horse Comics Crunchyroll directors superman Legendary Tumblr comiccon Television Critics Association binge worst movies facebook Paramount Network Action boxoffice mission: impossible Mary Poppins Returns Reality disaster NYCC cancelled APB Creative Arts Emmys spain social media Universal Calendar BAFTA E3 Premiere Dates Exclusive Video australia period drama composers The Arrangement hispanic SundanceTV children's TV medical drama 72 Emmy Awards Classic Film Fall TV San Diego Comic-Con indie singing competition cults unscripted FX rotten movies we love know your critic festivals worst christmas movies travel PlayStation award winner reviews DGA Comic Book video on demand WGN boxing Writers Guild of America Sneak Peek Set visit FOX strong female leads Box Office Avengers hollywood canceled TV shows MSNBC ITV The Walking Dead 20th Century Fox universal monsters a nightmare on elm street asian-american Awards Tour Discovery Channel women Fox News Logo Fantasy Watching Series E! YA toronto Baby Yoda Tubi 93rd Oscars Ghostbusters dramedy cancelled TV shows Tomatazos sequels Warner Bros. high school Oscars marvel cinematic universe Opinion diversity Rock japanese nfl Amazon Prime Video Spring TV kaiju The Witch war Countdown TV One Family zombies joker news tv talk aapi LGBT crime Lifetime Christmas movies Winners emmy awards live action revenge DC streaming service cancelled TV series 2019 Masterpiece dceu godzilla TBS mockumentary Academy Awards teaser Anna Paquin Hulu dogs Apple TV Red Carpet docudrama twilight police drama game show Valentine's Day Reality Competition Amazon Prime Animation blaxploitation pirates of the caribbean spy thriller talk show comedies TruTV Black History Month elevated horror Acorn TV movie historical drama vampires casting Television Academy video new star wars movies TCA Esquire Sci-Fi Crackle stoner serial killer 78th Annual Golden Globe Awards SXSW doctor who VICE Superheroes Cannes foreign 21st Century Fox YouTube adventure king kong golden globes Nickelodeon Schedule scary movies Photos Starz Certified Fresh based on movie heist movie comic thriller spanish language chucky Emmys Black Mirror Martial Arts new york Music crime drama james bond Heroines Columbia Pictures supernatural what to watch YouTube Red Disney Plus psychological thriller Trailer 4/20 Elton John GoT Cosplay See It Skip It ratings ESPN First Reviews Netflix Christmas movies TLC all-time Comedy obituary AMC Musical Ovation USA Network Comedy Central nbcuniversal sitcom Brie Larson Apple TV Plus critics renewed TV shows RT History VH1 PBS Biopics Nominations 2015 Funimation 2020 green book Adult Swim X-Men psycho Summer crime thriller MTV black politics latino robots rt archives Quiz Star Wars lord of the rings Character Guide Western mutant ABC Family President Star Trek Disney+ Disney Plus natural history french ViacomCBS Travel Channel Freeform Britbox BBC America Spike RT21 theme song indiana jones CNN OWN south america Holiday fresh superhero nature anime Women's History Month transformers Lucasfilm Nat Geo hidden camera Chernobyl classics versus blockbuster sequel Lifetime kong Peacock Marvel spanish discovery franchise cars Polls and Games Binge Guide streaming Hear Us Out dark Pacific Islander Podcast History Unbreakable Kimmy Schmidt Marathons documentaries cooking PaleyFest Rocketman spider-man Hallmark Christmas movies razzies die hard comics cartoon animated Pop TV Shudder FXX football canceled Mudbound Video Games Syfy true crime Apple TV+ witnail screen actors guild jamie lee curtis TIFF Pixar Sundance TV name the review sports Pride Month slashers Year in Review ABC Signature Drama 99% New York Comic Con YouTube Premium reboot American Society of Cinematographers spinoff DC Universe Fox Searchlight dc TCA 2017 Amazon OneApp italian golden globe awards Showtime DirecTV biography HBO docuseries Chilling Adventures of Sabrina Sundance Paramount Thanksgiving television concert best Walt Disney Pictures BET fast and furious Netflix 71st Emmy Awards remakes Winter TV CMT The Purge Pirates monster movies Extras movies venice TCA Awards scene in color deadpool National Geographic Mary Tyler Moore Pet Sematary 2017 HBO Max cops ID 2018 Disney streaming service Christmas sag awards scorecard political drama WarnerMedia Broadway Country 45 Election prank Captain marvel breaking bad dragons Grammys Marvel Television Shondaland miniseries Paramount Plus Sony Pictures blockbusters Sundance Now 24 frames romance Mindy Kaling FX on Hulu target Arrowverse BET Awards HBO Go Comics on TV quibi Lionsgate free movies Vudu Ellie Kemper Alien Stephen King Horror international USA VOD batman Superheroe Hallmark films technology screenings zero dark thirty ABC archives LGBTQ rotten satire halloween tv independent trailers Trophy Talk harry potter Epix rom-coms comic books saw Rocky cinemax The Walt Disney Company Song of Ice and Fire Infographic Marvel Studios halloween Mary poppins crossover A&E Cartoon Network BBC aliens Interview toy story Kids & Family CW Seed SDCC book Bravo Mystery Film Turner telelvision space Awards CBS TV Land romantic comedy Musicals Rom-Com"
     };
   },
   computed: {
+    valueIcon() {
+      const results = [];
+      if (this.value[0] >= 80)
+        results.push(
+          "https://live.staticflickr.com/65535/51280469111_d17f4e62ea_o.png"
+        );
+      else if (this.value[0] >= 60 && this.value[0] < 80)
+        results.push(
+          "https://live.staticflickr.com/65535/51280643468_b13c69ff22_o.png"
+        );
+      else
+        results.push(
+          "https://live.staticflickr.com/65535/51278099823_29be28c9b3_o.png"
+        );
+
+      if (this.value[1] >= 80)
+        results.push(
+          "https://live.staticflickr.com/65535/51280469111_d17f4e62ea_o.png"
+        );
+      else if (this.value[1] >= 60 && this.value[1] < 80)
+        results.push(
+          "https://live.staticflickr.com/65535/51280643468_b13c69ff22_o.png"
+        );
+      else
+        results.push(
+          "https://live.staticflickr.com/65535/51278099823_29be28c9b3_o.png"
+        );
+      return results;
+    },
     tags() {
       return this.tagsString.split(" ");
     }
@@ -583,6 +648,35 @@ export default {
     }
   },
   watch: {
+    value(val, old) {
+      if (JSON.stringify(val) !== JSON.stringify([80, 99]))
+        this.showFreshOnly = false;
+      if (JSON.stringify(val) !== JSON.stringify(old))
+        this.$apollo
+          .query({
+            query: getFlimsByParams,
+            variables: {
+              page: this.currentPage || 1,
+              size: this.perPage || 20,
+              type: "movie",
+              sortKey: this.sort || "Release Date",
+              params: {
+                range: this.value || [1, 99],
+                genres: this.selected.map(item => item.toLowerCase()) || [],
+                searchKey: this.filter || ""
+              }
+            }
+          })
+          .then(res => {
+            console.log("🚀 --------------------------");
+            console.log("🚀 ~ reloadFlims ~ res", res);
+            console.log("🚀 --------------------------");
+          })
+          .catch(() => {});
+    },
+    showFreshOnly(val) {
+      if (val) this.value = [80, 99];
+    },
     slug(val) {
       switch (val) {
         case "opening_this_week":
